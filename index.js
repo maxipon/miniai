3af4e9d63d6c5da8f1759ddf9f166d80b4ae0c12
maxipon <phi1077000@gmail.com>
2024-07-28 19:25:05 +0700

    moving index.js to fit with vercel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const summarizeText = require('../summarize.js');
+const summarizeText = require('./summarize.js');
 
 // Parses JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -45,5 +45,3 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-
-export default app;
